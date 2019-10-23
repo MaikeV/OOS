39311f71e700a15196a936d0ea25c5c45a1b8fcc
Maike <maike.voss7@gmail.com>
2019-10-23 19:58:58 +0200

    Praktikum 1 Aufgabe 1

---
+++ PascalschesDreieck/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        PascalschesDreieck triangle = new PascalschesDreieck(10);
+
+        triangle.fill();
+        triangle.print();
+    }
+}
---
+++ PascalschesDreieck/src/PascalschesDreieck.java
@@ -0,0 +1,33 @@
+public class PascalschesDreieck {
+    private int depth;
+    private int[][] triangle;
+
+    PascalschesDreieck() {}
+
+    PascalschesDreieck(int depth) {
+        this.depth = depth;
+        this.triangle = new int[depth][];
+    }
+
+    void fill() {
+        for(int i = 0; i < this.depth; i++) {
+            triangle[i] = new int[i+1];
+            for(int j = 0; j < i + 1; j++) {
+                if (j == 0 || j == i) {
+                    this.triangle[i][j] = 1;
+                } else {
+                    this.triangle[i][j] = this.triangle[i-1][j-1] + this.triangle[i-1][j];
+                }
+            }
+        }
+    }
+
+    void print() {
+        for(int i = 0; i < this.depth; i++) {
+            for(int j = 0; j < i + 1; j++) {
+                System.out.print(this.triangle[i][j] + "  ");
+            }
+            System.out.println("");
+        }
+    }
+}
